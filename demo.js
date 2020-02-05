9686d7172bff801ff65d05048ca7fbabc7d83d59
K1R5T <kirstypurkhardt@gmail.com>
2020-02-05 10:29:15 +0000

    new comments

--- demo.js
+++ demo.js
@@ -3,3 +3,9 @@ console.log("hello peoples");
 console.log("Today is a great day to smile")
 
 console.log("Don't worry be happy")
+
+console.log("Wednesday 5th Feb 2020")
+
+console.log("I L0V3 C0D3 !!")
+
+console.log("Work hard Play hard")
